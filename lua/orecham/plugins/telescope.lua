91717f0fa706d9b70c99a6602074b3996849a382
Jeff Ithier <jeff@ithier.dev>
2024-01-02 12:12:59 +0900

    add keybind to search buffers

--- lua/orecham/plugins/telescope.lua
+++ lua/orecham/plugins/telescope.lua
@@ -32,6 +32,6 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>fb", "<cmd>Telescope buffer<cr>", { desc = "Find in an open buffer" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find in an open buffer" })
   end,
 }
