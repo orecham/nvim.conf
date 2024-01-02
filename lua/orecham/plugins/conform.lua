3bbf43416b92ccc0034d40fcc9df317979f0ff24
Jeff Ithier <jeff@ithier.dev>
2024-01-02 06:06:01 +0100

    add conform for formatting

---
+++ lua/orecham/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        cpp = { "clang_format" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+  end,
+}
