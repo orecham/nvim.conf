a390e6ce10d8f1421e41a0a09ba3601b6395068c
Jeff Ithier <jeff@ithier.dev>
2024-07-19 00:06:51 +0200

    tweak tagbar

---
+++ lua/orecham/plugins/tagbar.lua
@@ -0,0 +1,8 @@
+return {
+  "preservim/tagbar",
+  config = function()
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<F8>", ":TagbarToggle<CR>", { desc = "Toggle tagbar visibility" })
+  end,
+}
