990debafee187df7940a8ed4658990ff57c7a052
Jeff Ithier <jeff@ithier.dev>
2024-07-19 17:37:33 +0200

    replace nvim-tree with neo-tree

---
+++ lua/orecham/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    local keymap = vim.keymap -- for conciseness
+    keymap.set("n", "<leader>ee", "<cmd>Neotree reveal toggle<cr>", { desc = "Toggle file explorer" })
+  end
+}
