2925e4418dc6c5c8ed34bcbe4970e6d08a6b4842
Jeff Ithier <jeff@ithier.dev>
2024-12-18 22:10:50 +0100

    Tweak keymaps

--- lua/orecham/core/keymaps.lua
+++ lua/orecham/core/keymaps.lua
@@ -16,8 +16,9 @@ vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decr
 -- window management
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
+vim.keymap.set("n", "<leader>sw", "<C-w>|", { desc = "Make split full screen" })                    -- make split windows equal width & height
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
+vim.keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
 
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
