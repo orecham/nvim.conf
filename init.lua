8d5564cdaff3e12eb70c77aa2b5fb048b0e567c5
Jeff Ithier <jeff@ithier.dev>
2025-12-28 04:17:16 +0100

    Integrate system clipboard on Wayland

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 require("orecham.core")
 require("orecham.lazy")
+
+-- Use system clipboard with Wayland
+if os.getenv("WAYLAND_DISPLAY") then
+  vim.g.clipboard = {
+    name = 'wl-clipboard',
+    copy = {
+      ['+'] = 'wl-copy',
+      ['*'] = 'wl-copy',
+    },
+    paste = {
+      ['+'] = 'wl-paste --no-newline',
+      ['*'] = 'wl-paste --no-newline',
+    },
+    cache_enabled = 0,
+  }
+end
