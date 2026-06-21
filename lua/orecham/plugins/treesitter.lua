aba99d469d36afa2a918b1d254e6c51dcae0777c
Jeff Ithier <jeff@ithier.dev>
2026-06-21 17:55:48 +0200

    fix treesitter config

--- lua/orecham/plugins/treesitter.lua
+++ lua/orecham/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.config")
+    local configs = require("nvim-treesitter.configs")
 
     configs.setup({
       ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "vimdoc", "vim" },
